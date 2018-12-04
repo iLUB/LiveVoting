341f9b925b34e4da293c416e3d12253fc2a2c36e
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2018-12-04 13:09:20 +0100

    updated for ILIAS 5.4

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'xlvo';
 $version = '3.6.3';
 $ilias_min_version = '5.2.0';
-$ilias_max_version = '5.3.999';
+$ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag';
 $responsible_mail = 'info at studer-raimann.ch';
